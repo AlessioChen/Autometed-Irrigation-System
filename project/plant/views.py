36d1c103f9b7712293ab564cecf1755442662c46
alessio <alessiochen98@gmail.com>
2020-10-21 19:19:15 +0200

    created on/off button

--- project/plant/views.py
+++ project/plant/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+
+def index(request):
+     
+     # Get a session value, setting a default if it is not present ('off')
+    motor_status = request.session.get('motor_status','off')
+
+    if motor_status == 'off' :  #turn on the motor 
+        request.session['motor_status'] = 'on'
+    else: 
+        request.session['motor_status'] = 'off'
+
+    
+    context = {
+        'motor_status': motor_status,
+    }
+
+
+
+    return render(request, 'plant/index.html', context )
