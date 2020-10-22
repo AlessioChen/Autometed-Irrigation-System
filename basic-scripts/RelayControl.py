8e5d65a9800b1c6cbb58dbd9c5798c1a0ee0b80d
GiacomoVitangeli <giacomo.vitangeli@gmail.com>
2020-10-22 17:37:17 +0200

    basic-scripts-00

---
+++ basic-scripts/RelayControl.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO #provides a class to control the GPIO
+import time
+
+in1 = 16 #PIN16, GPIO23
+
+GPIO.setmode(GPIO.BOARD) #GPIO.BOARD use PIN ID to init
+                         #GPIO.BCM use GPIO ID to init
+
+GPIO.setup(in1, GPIO.OUT) #set in1 as output digital line
+
+try:
+    while True:
+        
+        #TODO while(growHumidity < percent):
+            GPIO.output(in1, False) #Engine ON
+            time.sleep(2) #for Testing
+            
+        #TODO out of While(gH < p)
+            GPIO.output(in1, True) #Engine OFF
+            time.sleep(2) #for Testing
+
+
+except KeyboardInterrupt: 
+    GPIO.cleanup() #Emergency Engine OFF
